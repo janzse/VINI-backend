08840059d4edb55e2ba134fad653584e44a75cab
L. Stuckstette <lstuckstette@gmail.com>
2018-06-11 12:14:46 +0200

    updated dummy routes

--- src/authorisation/authRoutesMethods.js
+++ src/authorisation/authRoutesMethods.js
@@ -2,13 +2,13 @@ import userDBHelper from "../database/userDBHelper";
 
 /* handles the api call to register the user and insert them into the users table.
   The req body should contain an email and a password. */
-function registerUser(req, res){
+function registerUser(req, res) {
 
   console.log("registerUser: req.body is:", req.body);
 
   userDBHelper.doesUserExist(req.body.email, (doesUserExist) => {
 
-    if (doesUserExist){
+    if (doesUserExist) {
       res.status(400);
       res.json({
         "message": "User already exists"
@@ -19,13 +19,13 @@ function registerUser(req, res){
 
     userDBHelper.registerUserInDB(req.body.email, req.body.password, req.body.privateKey, req.body.authorityLevel, req.body.forename, req.body.surname, req.body.companyName, req.body.creationDate, req.body.blocked, (hasError, rowCount) => {
 
-      if(!hasError){
+      if (!hasError) {
         res.status(200);
         res.json({
           "message": "Registration was successful"
         })
       }
-      else{
+      else {
         res.status(500);
         res.json({
           "message": "Failed to register user due to a server error"
@@ -35,43 +35,90 @@ function registerUser(req, res){
   })
 }
 
-function deleteUser(req, res)
-{
-    console.log("registerUser: req.body is:", req.body);
+function deleteUser(req, res) {
+  console.log("registerUser: req.body is:", req.body);
 
-    userDBHelper.doesUserExist(req.body.email, (doesUserExist) =>
-    {
-        if (doesUserExist)
-        {
-            userDBHelper.deleteUserFromDB(req.body.email, result =>
-            {
-                if(result.length === 0){
-                    res.status(200);
-                    res.json({
-                        "message": "Deletion was successful"
-                    })
-                }
-                else{
-                    res.status(500);
-                    res.json({
-                        "message": "Failed to delete user due to a server error"
-                    })
-                }
-            })
+  userDBHelper.doesUserExist(req.body.email, (doesUserExist) => {
+    if (doesUserExist) {
+      userDBHelper.deleteUserFromDB(req.body.email, result => {
+        if (result.length === 0) {
+          res.status(200);
+          res.json({
+            "message": "Deletion was successful"
+          })
         }
-        else
-        {
-            res.status(400);
-            res.json({
-                "message": "User does not exists"
-            })
+        else {
+          res.status(500);
+          res.json({
+            "message": "Failed to delete user due to a server error"
+          })
+        }
+      })
+    }
+    else {
+      res.status(400);
+      res.json({
+        "message": "User does not exists"
+      })
 
-            return;
-        }
-    })
+      return;
+    }
+  })
 }
 
-function login(registerUserQuery, res){
+//DUMMY FUNCTION!!!!
+//VINI.de/api/users/get
+function getUser(req, res) {
+  var transactionPayload = [];
+
+
+  var payloadItem1 = {
+    date: "11.06.2008",
+    forename: "Ernst",
+    surname: "Mustermann",
+    authorityLevel: "tuev",
+    action: "dummy",
+    email: queryMail,
+    company: "TUEV"
+  };
+  var payloadItem2 = {
+    date: "11.06.2018",
+    forename: "Brigitte",
+    surname: "Mustermann",
+    authorityLevel: "zws",
+    action: "dummy",
+    email: queryMail,
+    company: "KFZ Bongard"
+  };
+  var payloadItem3 = {
+    date: "11.06.2018",
+    forename: "Johnathan",
+    surname: "Mustermann",
+    authorityLevel: "stva",
+    action: "dummy",
+    email: queryMail,
+    company: "Amt X"
+  };
+  var payloadItem4 = {
+    date: "12.06.2018",
+    forename: "Gabi",
+    surname: "Mustermann",
+    authorityLevel: "astva",
+    action: "dummy",
+    email: queryMail,
+    company: "Amt Y"
+  };
+
+  transactionPayload.push(payloadItem1);
+  transactionPayload.push(payloadItem2);
+  transactionPayload.push(payloadItem3);
+  transactionPayload.push(payloadItem4);
+
+  res.send(JSON.stringify(transactionPayload));
+  return;
+}
+
+function login(registerUserQuery, res) {
 
   console.log("User login successful");
 
@@ -81,16 +128,16 @@ let app;
 
 //TODO: Das herumreichen der "app" Instanz ist sehr unschön. FIXME!
 
-function isAuthorised(req, res, next){
+function isAuthorised(req, res, next) {
   const authResult = app.oauth.authorise()(req, res, next);
 
-  if(authResult.bearerToken != null){
+  if (authResult.bearerToken != null) {
     console.log("TOKEN: ", authResult.bearerToken);
 
     //TODO: Validierung der Nutzerrechte (authorisation Level)
     //TODO: Prüfen, ob der User deaktiviert ist
   }
-  else{
+  else {
     console.log("No valid accessToken found");
     res.status(403);
     res.redirect("/");
@@ -98,7 +145,7 @@ function isAuthorised(req, res, next){
 }
 
 module.exports = {
-  "setApp": (expressApp) => {app = expressApp},
+  "setApp": (expressApp) => { app = expressApp },
   "registerUser": registerUser,
   "login": login,
   "isAuthorised": isAuthorised,
