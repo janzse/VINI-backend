8522c6addb4004a60745c0d362d102b042970ccb
janzse <sebastian.janzen@fh-bielefeld.de>
2018-06-12 21:52:29 +0200

    Mergekonfikt beheben

--- src/database/msSqlWrapper.js
+++ src/database/msSqlWrapper.js
@@ -6,82 +6,82 @@ let dbConnection = null;
 
 function query(queryString, callback) {
 
-  if(dbConnection == null){
-    initConnection(queryString, callback);
-  }
-  else{
-    executeSql(queryString, callback);
-  }
+    if(dbConnection == null){
+        initConnection(queryString, callback);
+    }
+    else{
+        executeSql(queryString, callback);
+    }
 }
 
 function initConnection(query, callback) {
-  console.log("Initializing DB connection");
+    console.log("Initializing DB connection");
 
-  dbConnection = new Connection({
-    userName: 'vini@vini.database.windows.net',
-    password: 't51sy9RbdohKsa',
-    server: 'vini.database.windows.net',
-    options: {
-      encrypt: true,
-      database: 'vini-database'
-    }
-  });
+    dbConnection = new Connection({
+        userName: 'vini@vini.database.windows.net',
+        password: 't51sy9RbdohKsa',
+        server: 'vini.database.windows.net',
+        options: {
+            encrypt: true,
+            database: 'vini-database'
+        }
+    });
 
-  dbConnection.on('connect', function (err) {
-    if (err) {
-      console.log("Unable to connect to database!", err);
-    }
-    else {
-      console.log("Successfully connected to DB");
-      executeSql(query, callback);
-    }
-  });
+    dbConnection.on('connect', function (err) {
+        if (err) {
+            console.log("Unable to connect to database!", err);
+        }
+        else {
+            console.log("Successfully connected to DB");
+            executeSql(query, callback);
+        }
+    });
 
-  dbConnection.on('end', function () {
-    console.log("DB connection has been closed.");
-    dbConnection = null;
-  })
+    dbConnection.on('end', function () {
+        console.log("DB connection has been closed.");
+        dbConnection = null;
+    })
 }
 
 function executeSql(query, callback) {
 
-  let resultValues = [];
+    let resultValues = [];
 
-  const request = new Request(query, function (err, rowCount) {
-    if (err) {
-      console.log("Error while request was performed: ", err);
-      return;
-    }
-    else {
-      console.log("Got ", rowCount, " row(s)");
-    }
+    const request = new Request(query, function (err, rowCount) {
+        if (err) {
+            console.log("Error while request was performed: ", err);
+            return;
+        }
+        else {
+            console.log("Got ", rowCount, " row(s)");
+        }
 
-    dbConnection.close();
-  });
-
-  request.on('row', function (columns) {
-    // This collects all non-null rows in an array
-    columns.forEach(function (column) {
-      if (column.value != null) {
-        resultValues.push(column.value);
-      }
+        dbConnection.close();
     });
-  });
 
-  request.on('requestCompleted', function () {
-    callback(false, resultValues);
-  });
+    request.on('row', function (columns) {
+        // This collects all non-null rows in an array
+        columns.forEach(function (column) {
+            if (column.value != null) {
+                resultValues.push(column.value);
+            }
+        });
+    });
 
-  request.on('error', function (err) {
-    console.log("Error while executing ", query); // Might not be secure
-    callback(true, null);
-    throw err;
-  });
+    request.on('requestCompleted', function () {
+        callback(false, resultValues);
+    });
 
-  dbConnection.execSql(request);
+    request.on('error', function (err) {
+        console.log("Error while executing ", query); // Might not be secure
+        callback(true, null);
+        throw err;
+    });
+
+    dbConnection.execSql(request);
 }
 
 
 module.exports = {
-  "query": query
+    "query": query
 };
