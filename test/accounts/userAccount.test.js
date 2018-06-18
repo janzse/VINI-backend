1284f7c694062cc61b2d8bfd2b91ec0c16356334
Nils Dralle <nils.91@googlemail.com>
2018-06-18 14:45:53 +0200

    user account instanciate test and class fix

--- test/accounts/userAccount.test.js
+++ test/accounts/userAccount.test.js
@@ -1,3 +1,10 @@
 test('Init test case', () => {
     expect(1).toBe(1);
 });
+
+test('UserAccount class instanciate', () => {
+    const UserAccount=require("../../src/accounts/userAccount");
+
+    let t=new UserAccount();
+    expect(t).not.toBe('undefined');
+});
