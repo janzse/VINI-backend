f699a38516ff6fbe868afb7f2854c83667078705
Nils Dralle <nils.91@googlemail.com>
2018-06-18 16:45:01 +0200

    unit tests User account

--- test/accounts/userAccount.test.js
+++ test/accounts/userAccount.test.js
@@ -8,3 +8,39 @@ test('UserAccount class instanciate', () => {
     let t=new UserAccount();
     expect(t).toBeInstanceOf(UserAccount);
 });
+test('UserAccount set Blocked test', () => {
+    const UserAccount=require("../../src/accounts/userAccount");
+
+    let t=new UserAccount();
+    t.setBlocked(true);
+    expect(t.blocked).toBe(true);
+});
+test('UserAccount set Blocked false test', () => {
+    const UserAccount=require("../../src/accounts/userAccount");
+
+    let t=new UserAccount();
+    t.setBlocked(false);
+    expect(t.blocked).toBe(false);
+});
+test('UserAccount set Creation Date test', () => {
+    const UserAccount=require("../../src/accounts/userAccount");
+
+    let t=new UserAccount();
+    t.setCreationDate("1970-01-01");
+    expect(t.creationDate).toBe("1970-01-01");
+});
+test('UserAccount validate User test', () => {
+    const UserAccount=require("../../src/accounts/userAccount");
+
+    let t=new UserAccount();
+    t.password="secret";
+    expect(t.validateUser("secret")).toBe(true);
+});
+
+test('UserAccount dont validate User test', () => {
+    const UserAccount=require("../../src/accounts/userAccount");
+
+    let t=new UserAccount();
+    t.password="secret";
+    expect(t.validateUser("notsecret")).toBe(false);
+});
