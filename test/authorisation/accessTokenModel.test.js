9f37e0971c78731e9c8da0155d4e6881a10ec198
janzse <sebastian.janzen@fh-bielefeld.de>
2018-06-22 21:50:22 +0200

    Test: getUser

--- test/authorisation/accessTokenModel.test.js
+++ test/authorisation/accessTokenModel.test.js
@@ -1,3 +1,11 @@
+import {getUser} from "../__mocks__/request"
+
 test('Init test case', () => {
     expect(1).toBe(1);
+});
+
+test('Testing getUser', async () => {
+    expect.assertions(1);
+    const data = await getUser();
+    expect(data.email).toEqual('user@zws.com');
 });
